504b7e00437b12df509980f229e4e4763adeccaf
Mark Chweya <chweyamark@gmail.com>
2023-06-25 22:38:49 +0300

    Commit

---
+++ db/migrate/20230625193316_create_pizzas_table.rb
@@ -0,0 +1,9 @@
+class CreatePizzasTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pizzas do |t|
+      t.string :name
+      t.text :ingredients
+      t.timestamps
+    end
+  end
+end
