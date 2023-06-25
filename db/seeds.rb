9dc9d9791513a45451c39e0726620c658439ac12
Mark Chweya <chweyamark@gmail.com>
2023-06-25 22:02:48 +0300

    Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+Restaurant.create(name: 'Sottocasa NYC', address: '298 Atlantic Ave, Brooklyn, NY 11201')
+Restaurant.create(name: 'PizzArte', address: '69 W 55th St, New York, NY 10019')
+
+Pizza.create(name: 'Cheese', ingredients: 'Dough, Tomato Sauce, Cheese')
+Pizza.create(name: 'Pepperoni', ingredients: 'Dough, Tomato Sauce, Cheese, Pepperoni')
