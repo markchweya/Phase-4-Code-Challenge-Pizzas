13f38c588c42e639a204d70588968eb72652d133
Mark Chweya <chweyamark@gmail.com>
2023-06-25 22:25:31 +0300

    Commit

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -3,3 +3,10 @@ class RestaurantPizza < ApplicationRecord
     belongs_to :pizza
   end
   
+  class RestaurantPizza < ApplicationRecord
+    belongs_to :restaurant
+    belongs_to :pizza
+  
+    validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 30 }
+  end
+  
--- db/schema.rb
+++ db/schema.rb
@@ -1,14 +1,6 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
+# db/seeds.rb
+Restaurant.create(name: 'Sottocasa NYC', address: '298 Atlantic Ave, Brooklyn, NY 11201')
+Restaurant.create(name: 'PizzArte', address: '69 W 55th St, New York, NY 10019')
 
-ActiveRecord::Schema[7.0].define(version: 0) do
-end
+Pizza.create(name: 'Cheese', ingredients: 'Dough, Tomato Sauce, Cheese')
+Pizza.create(name: 'Pepperoni', ingredients: 'Dough, Tomato Sauce, Cheese, Pepperoni')
