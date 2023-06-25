ed38653031f0c9a3526cd79868ed788c9991a190
Mark Chweya <chweyamark@gmail.com>
2023-06-25 22:27:47 +0300

    Commit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,26 @@
 class RestaurantsController < ApplicationController
-end
+    def index
+      restaurants = Restaurant.all
+      render json: restaurants, only: [:id, :name, :address]
+    end
+  
+    def show
+      restaurant = Restaurant.find_by(id: params[:id])
+      if restaurant
+        render json: restaurant.as_json(include: { pizzas: { only: [:id, :name, :ingredients] } })
+      else
+        render json: { error: 'Restaurant not found' }, status: :not_found
+      end
+    end
+  
+    def destroy
+      restaurant = Restaurant.find_by(id: params[:id])
+      if restaurant
+        restaurant.destroy
+        head :no_content
+      else
+        render json: { error: 'Restaurant not found' }, status: :not_found
+      end
+    end
+  end
+  
